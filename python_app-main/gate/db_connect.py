707d8255e55387f5a9ec2cc886afe8bb052c54b3
Dmitry-milyaev <127052970+Dmitry-milyaev@users.noreply.github.com>
2023-03-12 20:53:43 +0300

    Delete python_app-main directory

--- python_app-main/gate/db_connect.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def connect_db():
- connection = psycopg2.connect(user="postgres",
-                              password="mysecretpassword",
-                              host="database",
-                              port="5432",
-                              database="app_db")
- return connection
